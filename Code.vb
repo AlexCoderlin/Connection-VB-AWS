55baba1a435165cbf6b5a37a42ca5e6d1973697a
Alejandro Gonzalez Colin <ekmmoproductions@gmail.com>
2020-05-06 13:49:37 -0500

    Se agregan espacios al final de cada linea del query SQL
    Se argregan espacios para solucionar error (80004005) VB.

--- Code.vb
+++ Code.vb
@@ -28,18 +28,18 @@ Sub Ejecutar_SQL_en_AWS()
     con.CommandTimeout = 900
 
     'Esta es la SQL que queremos consultar
-    sql = "SELECT    queue.name," & _
-    "initiationmethod," & _
-    "CASE WHEN agent is not null THEN 1 ELSE 0 END as Agente," & _
-    "replace(replace(queue.enqueuetimestamp,'T',' '),'Z','') as enqueuetimestamp," & _
-    "replace(replace(agent.connectedtoagenttimestamp,'T',' '),'Z','') as connectedtoagenttimestamp," & _
-    "replace(replace(disconnecttimestamp,'T',' '),'Z','') as disconnecttimestamp," & _
-    "queue.duration," & _
-    "agent.agentinteractionduration," & _
-    "agent.username" & _
-    "FROM AwsDataCatalog.asd_amr_db.all_ctr_data" & _
-    "WHERE substring(queue.name,1,5) = 'Femsa'" & _
-    "And cast(replace(replace(queue.enqueuetimestamp,'T',' '),'Z','') as timestamp) between cast('2020-04-28 05:00:00' as timestamp) and cast('2020-04-29 04:59:59' as timestamp)" & _
+    sql = "SELECT    queue.name, " & _
+    "initiationmethod, " & _
+    "CASE WHEN agent is not null THEN 1 ELSE 0 END as Agente, " & _
+    "replace(replace(queue.enqueuetimestamp,'T',' '),'Z','') as enqueuetimestamp, " & _
+    "replace(replace(agent.connectedtoagenttimestamp,'T',' '),'Z','') as connectedtoagenttimestamp, " & _
+    "replace(replace(disconnecttimestamp,'T',' '),'Z','') as disconnecttimestamp, " & _
+    "queue.duration, " & _
+    "agent.agentinteractionduration, " & _
+    "agent.username " & _
+    "FROM AwsDataCatalog.asd_amr_db.all_ctr_data " & _
+    "WHERE substring(queue.name,1,5) = 'Femsa' " & _
+    "And cast(replace(replace(queue.enqueuetimestamp,'T',' '),'Z','') as timestamp) between cast('2020-04-28 05:00:00' as timestamp) and cast('2020-04-29 04:59:59' as timestamp) " & _
     "order by cast(replace(replace(queue.enqueuetimestamp,'T',' '),'Z','') as timestamp)"
 
     'Lanzamos la SQL
